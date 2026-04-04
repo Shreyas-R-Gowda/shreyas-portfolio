8e80c8b53c525e5db8968fa848406fde8b9adc4d
Shreyas R Gowda <shreyasr@users.noreply.github.com>
2026-04-04 15:20:59 +0530

    fix: resolve 404 asset paths for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/Portfolio-Website/", // Uncomment if hosting at 'username.github.io/Portfolio-Website/'
+  base: "./",
 });
